f00afcdf9ab6db7d111d74498af693dc8ef63502
AntonioCC147 <77618117+AntonioCC147@users.noreply.github.com>
2024-03-01 14:14:55 +0200

    Added animation

--- frontend/src/components/Main/Main.js
+++ frontend/src/components/Main/Main.js
@@ -1,3 +1,5 @@
+import { useInView } from 'react-intersection-observer';
+
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
@@ -11,6 +13,11 @@ import SocialIcons from '../SocialIcons/SocialIcons';
 import './Main.css'
 
 export default function Main() {
+    const [ref, inView] = useInView({
+        triggerOnce: true,
+        threshold: 0.5,
+    });
+
     return (
         <div className="containerMain" id="home">
             <NavbarComponent/>
@@ -23,7 +30,7 @@ export default function Main() {
                     <p className="title4Main">Lorem ipsum dolor sit amet, consetetur sadipscing elitr sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam.</p>
                     <Button className="buttonMain">Hire me</Button>
                 </Col>
-                <Col sm={12} md={6} className="imageContainer">
+                    <Col sm={12} md={6} ref={ref} className={`imageContainerWrapper ${inView ? 'imageContainerWrapperVisible' : ''}`}>
                     <img src={Avatar} className="imageMain" alt="Avatar"/>
                 </Col>
             </Row>
