20e0d2ac5a24de072082468d43944498777a82b6
AntonioCC147 <77618117+AntonioCC147@users.noreply.github.com>
2024-03-08 14:05:08 +0200

    Added new description

--- frontend/src/components/Pricing/Pricing.js
+++ frontend/src/components/Pricing/Pricing.js
@@ -26,101 +26,113 @@ export default function Pricing() {
             <div ref={ref} className={`containerBoxPricing pricingContainerWrapper ${inView ? 'pricingContainerWrapperVisible' : ''}`}>
                 <div class="cardBoxPricing" style={{marginTop: "50px", marginBottom: "25px"}}>
                     <p className="cardTitle">Basic</p>
-                    <p className="cardPrice">100 lei</p>
-                    <p className="cardDescription">Ceva text aici</p>
+                    <p className="cardPrice">25 EUR</p>
+                    <p className="cardDescription">1 basic text logo without any drawing or symbol + png</p>
                     <hr/>
                     <div style={{paddingBottom: "15px"}}>
                         <img src={Verify} className="imageVerify" alt="Verify"/>
-                        <span className="cardTextBenefits">Minimalist logo</span>
+                        <span className="cardTextBenefits">1 concept included</span>
                     </div>
                     <div style={{paddingBottom: "15px"}}>
                         <img src={Verify} className="imageVerify" alt="Verify"/>
-                        <span className="cardTextBenefits">Simple design</span>
+                        <span className="cardTextBenefits">Logo transparency</span>
+                    </div>
+                    <div style={{paddingBottom: "15px"}}>
+                        <img src={Verify} className="imageVerify" alt="Verify"/>
+                        <span className="cardTextBenefits">Printable file</span>
                     </div>
                     <div style={{paddingBottom: "15px"}}>
                         <img src={NotVerify} className="imageVerify" alt="Verify"/>
-                        <span className="cardTextBenefits">Creative logo</span>
+                        <span className="cardTextBenefits">Include source file</span>
                     </div>
                     <div style={{paddingBottom: "15px"}}>
                         <img src={NotVerify} className="imageVerify" alt="Verify"/>
-                        <span className="cardTextBenefits">Colored design</span>
+                        <span className="cardTextBenefits">Include 3D mockup</span>
                     </div>
                     <div style={{paddingBottom: "15px"}}>
                         <img src={NotVerify} className="imageVerify" alt="Verify"/>
-                        <span className="cardTextBenefits">Animated logo</span>
+                        <span className="cardTextBenefits">Include social media kit</span>
                     </div>
                     <div style={{paddingBottom: "15px"}}>
                         <img src={NotVerify} className="imageVerify" alt="Verify"/>
-                        <span className="cardTextBenefits">Premium design</span>
+                        <span className="cardTextBenefits">Vector file</span>
                     </div>
                     <div className="text-center">
-                        <Button className="buttonPricing">Choose Plan</Button>
+                        <Button className="buttonPricing" href="#contact">Choose Plan</Button>
                     </div>
                 </div>
                 <div class="cardBoxPricing" style={{marginTop: "25px", marginBottom: "50px"}}>
                     <p className="cardTitle">Standard</p>
-                    <p className="cardPrice">200 lei</p>
-                    <p className="cardDescription">Ceva text aici</p>
+                    <p className="cardPrice">35 EUR</p>
+                    <p className="cardDescription">2 logo design concept, JPG + PNG + PDF + Ai source file + 3D mockup</p>
                     <hr/>
                     <div style={{paddingBottom: "15px"}}>
                         <img src={Verify} className="imageVerify" alt="Verify"/>
-                        <span className="cardTextBenefits">Minimalist logo</span>
+                        <span className="cardTextBenefits">2 concept included</span>
                     </div>
                     <div style={{paddingBottom: "15px"}}>
                         <img src={Verify} className="imageVerify" alt="Verify"/>
-                        <span className="cardTextBenefits">Simple design</span>
+                        <span className="cardTextBenefits">Logo transparency</span>
                     </div>
                     <div style={{paddingBottom: "15px"}}>
                         <img src={Verify} className="imageVerify" alt="Verify"/>
-                        <span className="cardTextBenefits">Creative logo</span>
+                        <span className="cardTextBenefits">Printable file</span>
                     </div>
                     <div style={{paddingBottom: "15px"}}>
                         <img src={Verify} className="imageVerify" alt="Verify"/>
-                        <span className="cardTextBenefits">Colored design</span>
+                        <span className="cardTextBenefits">Include source file</span>
+                    </div>
+                    <div style={{paddingBottom: "15px"}}>
+                        <img src={Verify} className="imageVerify" alt="Verify"/>
+                        <span className="cardTextBenefits">Include 3D mockup</span>
                     </div>
                     <div style={{paddingBottom: "15px"}}>
                         <img src={NotVerify} className="imageVerify" alt="Verify"/>
-                        <span className="cardTextBenefits">Animated logo</span>
+                        <span className="cardTextBenefits">Include social media kit</span>
                     </div>
                     <div style={{paddingBottom: "15px"}}>
                         <img src={NotVerify} className="imageVerify" alt="Verify"/>
-                        <span className="cardTextBenefits">Premium design</span>
+                        <span className="cardTextBenefits">Vector file</span>
                     </div>
                     <div className="text-center">
-                        <Button className="buttonPricing">Choose Plan</Button>
+                        <Button className="buttonPricing" href="#contact">Choose Plan</Button>
                     </div>
                 </div>
                 <div className="cardBoxPricing" style={{marginTop: "0%"}}>
                     <p className="cardTitle">Premium</p>
-                    <p className="cardPrice">300 lei</p>
-                    <p className="cardDescription">Ceva text aici</p>
+                    <p className="cardPrice">60 EUR</p>
+                    <p className="cardDescription">3 detalied logo concepts JPG + PNG + EPS vector source + social media kit</p>
                     <hr/>
                     <div style={{paddingBottom: "15px"}}>
                         <img src={Verify} className="imageVerify" alt="Verify"/>
-                        <span className="cardTextBenefits">Minimalist logo</span>
+                        <span className="cardTextBenefits">3 concept included</span>
                     </div>
                     <div style={{paddingBottom: "15px"}}>
                         <img src={Verify} className="imageVerify" alt="Verify"/>
-                        <span className="cardTextBenefits">Simple design</span>
+                        <span className="cardTextBenefits">Logo transparency</span>
                     </div>
                     <div style={{paddingBottom: "15px"}}>
                         <img src={Verify} className="imageVerify" alt="Verify"/>
-                        <span className="cardTextBenefits">Creative logo</span>
+                        <span className="cardTextBenefits">Printable file</span>
                     </div>
                     <div style={{paddingBottom: "15px"}}>
                         <img src={Verify} className="imageVerify" alt="Verify"/>
-                        <span className="cardTextBenefits">Colored design</span>
+                        <span className="cardTextBenefits">Include source file</span>
                     </div>
                     <div style={{paddingBottom: "15px"}}>
                         <img src={Verify} className="imageVerify" alt="Verify"/>
-                        <span className="cardTextBenefits">Animated logo</span>
+                        <span className="cardTextBenefits">Include 3D mockup</span>
                     </div>
                     <div style={{paddingBottom: "15px"}}>
                         <img src={Verify} className="imageVerify" alt="Verify"/>
-                        <span className="cardTextBenefits">Premium design</span>
+                        <span className="cardTextBenefits">Include social media kit</span>
+                    </div>
+                    <div style={{paddingBottom: "15px"}}>
+                        <img src={Verify} className="imageVerify" alt="Verify"/>
+                        <span className="cardTextBenefits">Vector file</span>
                     </div>
                     <div className="text-center">
-                        <Button className="buttonPricing">Choose Plan</Button>
+                        <Button className="buttonPricing" href="#contact">Choose Plan</Button>
                     </div>
                 </div>
             </div>
